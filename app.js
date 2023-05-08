a6389d053ce2064833f9f8f2d16e827479b2da75
nikitaaldashkov <nikitastart@yandex.ru>
2023-05-08 18:46:19 +0300

    Simple example Hello world msg on express

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 import chalk from 'chalk';
+import express from 'express';
 
-console.log(chalk.blue('Hello world!'));
+const app = express();
+const port = 3000;
+app.get('/', function (req, res) {
+	res.send('<h1>Success</h1>');
+})
+
+app.listen(port, function () {
+	console.log(chalk.blue(`Server has been started on port ${port}`));
+});
